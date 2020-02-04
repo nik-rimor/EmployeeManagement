7a60c27162d9b83772e73c2854a49e4c0d05a14b
Nikos Kekes <nickkekes@gmail.com>
2020-02-04 19:12:35 +0200

    exhibit a middleware execution

--- EmployeeManagement/EmployeeManagement/Startup.cs
+++ EmployeeManagement/EmployeeManagement/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace EmployeeManagement
 {
@@ -25,7 +26,8 @@ namespace EmployeeManagement
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env,
+                              ILogger<Startup> logger)
         {
             if (env.IsDevelopment())
             {
@@ -34,15 +36,41 @@ namespace EmployeeManagement
 
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
+            app.Use(async (context, next) =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response
-                        .WriteAsync(System.Diagnostics.Process.GetCurrentProcess().ProcessName +
-                                    "\n" + "MyKey : " + _config["MyKey"]);
-                });
+                logger.LogInformation("M1: request received");
+                await context.Response.WriteAsync("Hello from 1st Middleware!");
+                await next();
+                logger.LogInformation("M1: response produced");
             });
+
+            app.Use(async (context, next) =>
+            {
+                logger.LogInformation("M2: request received");
+                await context.Response.WriteAsync("Hello from 2nd Middleware!");
+                await next();
+                logger.LogInformation("M2: response produced");
+            });
+
+            app.Run(async context =>
+            {
+                logger.LogInformation("Request reqceiver from Terminal middleware");
+                await context.Response.WriteAsync("Hello from 3rd Middleware!");
+                logger.LogInformation("Response generated from Terminal midleware");
+            });
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //endpoints.MapGet("/", async context =>
+            //{
+            //    logger.LogInformation("MW1: Incoming Rerquest");
+            //    await context.Response
+            //        .WriteAsync(System.Diagnostics.Process.GetCurrentProcess().ProcessName +
+            //                    "\n" + "MyKey : " + _config["MyKey"]);
+            //    logger.LogInformation("MW1: Outgoing Response");
+            //});
+
+            //});
         }
     }
 }
