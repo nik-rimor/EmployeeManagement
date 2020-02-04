bc2caa441839795b9e0a687003112b2b6fbebc4f
Nikos Kekes <nickkekes@gmail.com>
2020-02-04 21:37:37 +0200

    setup simple MVC requirements

--- EmployeeManagement/EmployeeManagement/Startup.cs
+++ EmployeeManagement/EmployeeManagement/Startup.cs
@@ -23,6 +23,7 @@ namespace EmployeeManagement
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvcCore(option => option.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,36 +31,14 @@ namespace EmployeeManagement
         {
             if (env.IsDevelopment())
             {
-                DeveloperExceptionPageOptions developerExceptionPageOptions = new DeveloperExceptionPageOptions
-                {
-                    SourceCodeLineCount = 10
-                };
+
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
 
-            app.UseFileServer();
+            app.UseMvcWithDefaultRoute();
 
-            app.UseRouting();
-
-            app.Run(async context =>
-            {
-                throw new Exception("Some error processing the request");
-                await context.Response.WriteAsync("Hello world!");
-            });
-
-            //app.UseEndpoints(endpoints =>
-            //{
-            //endpoints.MapGet("/", async context =>
-            //{
-            //    logger.LogInformation("MW1: Incoming Rerquest");
-            //    await context.Response
-            //        .WriteAsync(System.Diagnostics.Process.GetCurrentProcess().ProcessName +
-            //                    "\n" + "MyKey : " + _config["MyKey"]);
-            //    logger.LogInformation("MW1: Outgoing Response");
-            //});
-
-            //});
         }
     }
 }
