1ab20b5a9a060b8ae255528daa1758f200cbd36a
Nikos Kekes <nickkekes@gmail.com>
2020-02-06 22:08:24 +0200

    add seed data in AppDbContext for Employee

---
+++ EmployeeManagement/EmployeeManagement/Migrations/20200206200557_seed_Employee_data.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmployeeManagement.Migrations
+{
+    public partial class seed_Employee_data : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "Department", "Email", "Name" },
+                values: new object[] { 1, 2, "maria@testdom.test", "Maria" });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "Department", "Email", "Name" },
+                values: new object[] { 2, 3, "spyros@testdom.test", "Spyros" });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "Department", "Email", "Name" },
+                values: new object[] { 3, 3, "gina@testdom.test", "Gina" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
--- EmployeeManagement/EmployeeManagement/Models/AppDbContext.cs
+++ EmployeeManagement/EmployeeManagement/Models/AppDbContext.cs
@@ -16,5 +16,15 @@ namespace EmployeeManagement.Models
         }
 
         public DbSet<Employee> Employees { get; set; }
+
+        // Seed Data to Employees Entity in database
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().HasData(
+                    new Employee { Id = 1, Name = "Maria", Department = Dept.HR, Email = "maria@testdom.test" },
+                    new Employee { Id = 2, Name = "Spyros", Department = Dept.IT, Email = "spyros@testdom.test" },
+                    new Employee { Id = 3, Name = "Gina", Department = Dept.IT, Email = "gina@testdom.test" }
+                );
+        }
     }
 }
