cb2f82ef106635e56b6db379c4ecd0dd9c7c8faa
Nikos Kekes <nickkekes@gmail.com>
2020-02-06 19:31:23 +0200

    setup EF Core use with the application

--- EmployeeManagement/EmployeeManagement/Startup.cs
+++ EmployeeManagement/EmployeeManagement/Startup.cs
@@ -6,6 +6,7 @@ using EmployeeManagement.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +25,9 @@ namespace EmployeeManagement
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddDbContextPool<AppDbContext>(options =>
+                            options.UseSqlServer(_config.GetConnectionString("EmployeeDBConnection")));
             services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
 
             //services.AddMvc(option => option.EnableEndpointRouting = false);
