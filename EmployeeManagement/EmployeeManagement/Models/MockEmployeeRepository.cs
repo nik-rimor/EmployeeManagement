f36946cf5b4b741304f4828c8be103a6cab79dce
Nikos Kekes <nickkekes@gmail.com>
2020-02-06 10:39:34 +0200

    add nonstyled form for Create action on HomeController

--- EmployeeManagement/EmployeeManagement/Models/MockEmployeeRepository.cs
+++ EmployeeManagement/EmployeeManagement/Models/MockEmployeeRepository.cs
@@ -13,9 +13,9 @@ namespace EmployeeManagement.Models
         {
             _employeeList = new List<Employee>
             {
-                new Employee { Id = 1, Name = "Maria", Department = "HR", Email = "maria@testdom.test" },
-                new Employee { Id = 2, Name = "Spyros", Department = "IT", Email = "spyros@testdom.test" },
-                new Employee { Id = 3, Name = "Gina", Department = "IT", Email = "gina@testdom.test" },
+                new Employee { Id = 1, Name = "Maria", Department = Dept.HR, Email = "maria@testdom.test" },
+                new Employee { Id = 2, Name = "Spyros", Department = Dept.IT, Email = "spyros@testdom.test" },
+                new Employee { Id = 3, Name = "Gina", Department = Dept.IT, Email = "gina@testdom.test" },
             };
         }
 
